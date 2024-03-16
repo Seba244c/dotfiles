518776e3a4f4b4dd7e2a610cf569b50aedcc5bc0
Sebastian Snoer <ssnoer@proton.me>
2024-03-16 15:00:17 +0100

    telescope updates

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,32 @@
 return {
     {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local builtin = require("telescope.builtin")
-            vim.keymap.set('n', '<C-n>', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        end
+            vim.keymap.set("n", "<C-n>", builtin.git_files, {})
+            vim.keymap.set("n", "<leader>fn", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+            vim.keymap.set("n", "<leader>ff", function()
+                vim.ui.input({ prompt = "Workspace symbols: " }, function(query)
+                    builtin.lsp_workspace_symbols({ query = query })
+                end)
+            end, { desc = "LSP workspace symbols" })
+        end,
     },
     {
-        'nvim-telescope/telescope-ui-select.nvim',
+        "nvim-telescope/telescope-ui-select.nvim",
         config = function()
-            require("telescope").setup {
+            require("telescope").setup({
                 extensions = {
                     ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
-                        }
-                    }
-                }
-            }
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
             require("telescope").load_extension("ui-select")
-        end
-    }
+        end,
+    },
 }
