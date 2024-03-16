70848f81a30f3d7021d8a06e81d123a9c2b257f2
Sebastian Snoer <ssnoer@proton.me>
2024-03-16 15:16:39 +0100

    jdtls for multiple files.lool

---
+++ dot_config/nvim/after/ftplugin/java.lua
@@ -0,0 +1,14 @@
+local jdtls_bin = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
+local lombok_jar = vim.fn.stdpath("data") .. "/mason/share/jdtls/lombok.jar"
+
+local lsp_attach = function(client, bufnr)
+    -- mappings here
+end
+
+local config = {
+    cmd = { jdtls_bin, "--jvm-arg=" .. string.format("-javaagent:%s", lombok_jar) },
+    root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+    on_attach = lsp_attach,
+}
+
+require("jdtls").start_or_attach(config)
