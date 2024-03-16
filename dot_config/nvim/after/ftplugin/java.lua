010a2a630cfacf3b486a7bbdde9ed6beed05f8c8
Sebastian Snoer <ssnoer@proton.me>
2024-03-16 16:57:27 +0100

    dap

--- dot_config/nvim/after/ftplugin/java.lua
+++ dot_config/nvim/after/ftplugin/java.lua
@@ -1,14 +1,20 @@
 local jdtls_bin = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
 local lombok_jar = vim.fn.stdpath("data") .. "/mason/share/jdtls/lombok.jar"
+local dap_jar = vim.fn.stdpath("data") .. "/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin-*.jar"
 
 local lsp_attach = function(client, bufnr)
     -- mappings here
+require('jdtls.dap').setup_dap_main_class_configs()
 end
 
 local config = {
     cmd = { jdtls_bin, "--jvm-arg=" .. string.format("-javaagent:%s", lombok_jar) },
     root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
     on_attach = lsp_attach,
+    init_options = {
+		bundles = {
+            vim.fn.glob(dap_jar, 1) },
+	},
 }
 
 require("jdtls").start_or_attach(config)
--- dot_config/nvim/lua/plugins/jdtls.lua
+++ dot_config/nvim/lua/plugins/jdtls.lua
@@ -1,17 +1,5 @@
-local jdtls_bin = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
-local lombok_jar = vim.fn.stdpath("data") .. "/mason/share/jdtls/lombok.jar"
-
-local lsp_attach = function(client, bufnr)
-    -- mappings here
-end
-
-local config = {
-    cmd = { jdtls_bin, "--jvm-arg=" .. string.format("-javaagent:%s", lombok_jar) },
-    root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-    on_attach = lsp_attach,
-}
-
 return {
-    "mfussenegger/nvim-jdtls",
-    ft = "java",
+	"mfussenegger/nvim-jdtls",
+    dependencies = { "mfussenegger/nvim-dap" },
+	ft = "java",
 }
