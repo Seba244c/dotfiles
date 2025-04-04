ca6829d8102c3c6c67dd29fdccc5306b7d3a5254
Sebastian Snoer <ssnoer@proton.me>
2025-04-04 18:05:03 +0200

    fix(debug): Fix debug symbols

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -64,6 +64,32 @@ return {
 
 			vim.keymap.set("n", "<C-S-j>", "<cmd>lua require('dap').down()<cr>")
 			vim.keymap.set("n", "<C-S-k>", "<cmd>lua require('dap').up()<cr>")
+
+			-- Icons
+			vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#993939", bg = "#31353f" })
+			vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+			vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+
+			vim.fn.sign_define(
+				"DapBreakpoint",
+				{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+			)
+			vim.fn.sign_define(
+				"DapBreakpointCondition",
+				{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+			)
+			vim.fn.sign_define(
+				"DapBreakpointRejected",
+				{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+			)
+			vim.fn.sign_define(
+				"DapLogPoint",
+				{ text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+			)
+			vim.fn.sign_define(
+				"DapStopped",
+				{ text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" }
+			)
 		end,
 	},
 }
